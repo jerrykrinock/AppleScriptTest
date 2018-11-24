e27b6b9816bd155c71ded89afa6ed28d7abf667b
Jerry Krinock <jerry@sheepsystems.com>
2018-11-23 18:29:17 -0800

    Initial Commit

---
+++ AppleScriptTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AppleScriptTest
+//
+//  Created by Jerry on 18-11-23.
+//  Copyright © 2018 Jerry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ AppleScriptTest/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  AppleScriptTest
+//
+//  Created by Jerry on 18-11-23.
+//  Copyright © 2018 Jerry. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
