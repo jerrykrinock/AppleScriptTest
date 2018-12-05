15d6ad019fa70e3f3a32d00747e7b0f3241b4bc2
Jerry Krinock <jerry@sheepsystems.com>
2018-12-05 08:36:51 -0800

    • Now uses NSUserAppleScriptTask instead of NSAppleScript.  This seems to be the ticket to make it work in Mojave. • #if option SIMPLE to install scripts in ~/Library/Application Scripts, and run from there, or not. • Removed stupid check for codesigning. • Cosmetics

--- AppleScriptTest/AppDelegate.m
+++ AppleScriptTest/AppDelegate.m
@@ -1,6 +1,100 @@
 #import "AppDelegate.h"
 
+#if 11
+#warning Will run scripts directly from app product's Resources
+#define SIMPLE 1
+#else
+#warning Will install scripts in ~/Library/Application Scripts and run from there
+#endif
+
 @implementation AppDelegate
+
+- (NSURL*)scriptUrlForScriptName:(NSString*)scriptName {
+    NSError* error = nil;
+
+#if SIMPLE
+        NSURL* scriptUrl = [[NSBundle mainBundle] URLForResource:scriptName
+                                                   withExtension:@"scpt"];
+        if (!scriptUrl) {
+            error = [NSError errorWithDomain:[[NSBundle mainBundle] bundleIdentifier]
+                                        code:10003
+                                    userInfo:@{
+                                               NSLocalizedDescriptionKey : NSLocalizedString(@"Script missing from app package?", nil)
+                                               }];
+            NSLog(@"error 13: %@", error);
+        }
+
+        return scriptUrl;
+#else
+    NSURL* destinDir = [[NSFileManager defaultManager] URLForDirectory:NSApplicationScriptsDirectory
+                                                              inDomain:NSUserDomainMask
+                                                     appropriateForURL:nil
+                                                                create:YES
+                                                                 error:&error];
+    /* At this point destinDir is:
+     ~/Library/Application Scripts/com.sheepsystems.AppleScriptTest
+     */
+    if (error) {
+        NSLog(@"error 2: %@", error);
+    }
+
+    NSURL* scriptUrl = nil;
+    if (!error) {
+        scriptUrl = [destinDir URLByAppendingPathComponent:scriptName];
+        scriptUrl = [scriptUrl URLByAppendingPathExtension:@"scpt"];
+    }
+
+    return scriptUrl;
+#endif
+}
+
+#if SIMPLE
+#else
+- (void)installScriptWithName:(NSString*)scriptName {
+    NSError* error = nil;
+    NSURL* sourceFile = nil;
+
+    sourceFile  = [[NSBundle mainBundle] URLForResource:scriptName
+                                          withExtension:@"scpt"];
+    if (!sourceFile) {
+        error = [NSError errorWithDomain:[[NSBundle mainBundle] bundleIdentifier]
+                                    code:10003
+                                userInfo:@{
+                                           NSLocalizedDescriptionKey : NSLocalizedString(@"Script missing from app package?", nil)
+                                           }];
+        NSLog(@"error 3: %@", error);
+    }
+
+    if (!error) {
+        NSURL* destination = [self scriptUrlForScriptName:scriptName];
+		/* Maybe it would be cheaper to diff the existing installed file with
+         our resource and only do this if there is a difference (presumably
+         due to an update of the app.  But, whatever, this is guaranteed to
+         work… */
+		[[NSFileManager defaultManager] removeItemAtURL:destination
+                                                  error:NULL];
+        /* Ignored error since file might not exist. */
+
+        [[NSFileManager defaultManager] copyItemAtURL:sourceFile
+                                                toURL:destination
+                                                error:&error];
+        if (error) {
+            NSLog(@"error 4: %@", error);
+        } else {
+            NSLog(@"Succeeded installing Safari.scpt");
+        }
+    }
+}
+#endif
+
+
+- (void)applicationDidFinishLaunching:(NSNotification *)notification {
+#if SIMPLE
+#else
+    [self installScriptWithName:@"Safari"];
+    [self installScriptWithName:@"TextEdit"];
+#endif
+}
 
 - (IBAction)testSafari:(id)sender {
     [self testAppNamed:@"Safari"];
@@ -11,25 +105,39 @@
 }
 
 - (void)testAppNamed:(NSString*)appName {
-    NSURL* scriptUrl = [[NSBundle mainBundle] URLForResource:appName
-                                               withExtension:@"scpt"];
-    NSDictionary* errorDic = nil;
-    NSAppleScript* script = [[NSAppleScript alloc] initWithContentsOfURL:scriptUrl
-                                                                   error:&errorDic];
-    /*SSYDBL*/ NSLog(@"script source: %@", script.source) ;
-
-    if (script) {
-        self.textField.stringValue = @"Waiting…";
-        NSDictionary* error = nil;
-        NSAppleEventDescriptor* descriptor = [script executeAndReturnError:&error];
-        NSMutableString* report = [NSMutableString new];
-        [report appendString:@"RESULT:\n"];
-        [report appendFormat:@"Script returned error: %@\n", [error objectForKey:NSAppleScriptErrorMessage]];
-        [report appendFormat:@"Script returned data: %@", descriptor];
-        self.textField.stringValue = [report copy];
+    NSError* error = nil;
+    NSUserAppleScriptTask* script = [[NSUserAppleScriptTask alloc] initWithURL:[self scriptUrlForScriptName:appName]
+                                                                         error:&error];
+    if (error) {
+        NSLog(@"error 10: %@", error);
+        [self updateResult:@"Failed loading script resource"
+                scriptName:appName];
     } else {
-        self.textField.stringValue = @"Failed loading script resource";
+        [script executeWithAppleEvent:nil
+                    completionHandler:^(NSAppleEventDescriptor * _Nullable descriptor, NSError * _Nullable scriptError) {
+                        if (!scriptError) {
+                            NSMutableString* report = [NSMutableString new];
+                            [report appendFormat:@"DATA: %@", descriptor];
+                            [report appendString:@"\n\n"];
+                            [report appendFormat:@"ERROR: %@", scriptError];
+                            [self updateResult:[report copy]
+                                    scriptName:appName];
+                        } else {
+                            NSLog(@"error 12: %@", scriptError);
+                        }
+                        }];
     }
+}
+
+- (void)updateResult:(NSString*)result
+          scriptName:(NSString*)scriptName {
+    NSString* string = [[NSString alloc] initWithFormat:
+                        @"Ran: %@\n\nRESULT:\n\n%@",
+                        [[self scriptUrlForScriptName:scriptName] path],
+                        result];
+    [self.textField performSelectorOnMainThread:@selector(setStringValue:)
+                                     withObject:string
+                                  waitUntilDone:NO];
 }
 
 @end
